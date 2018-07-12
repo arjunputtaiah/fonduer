3300534d414b9fc9153a39ce305a87920282afd7
Luke Hsiao <lwhsiao@stanford.edu>
2018-07-12 14:25:20 -0700

    Fix the parse style test

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -18,7 +18,7 @@ from fonduer.parser.spacy_parser import Spacy
 ATTRIBUTE = "parser_test"
 
 
-def test_parse_results(caplog):
+def test_parse_md_details(caplog):
     """Unit test of the final results stored in the database of the md document.
 
     This test only looks at the final results such that the implementation of
@@ -47,6 +47,21 @@ def test_parse_results(caplog):
 
     # Grab the md document
     doc = session.query(Document).order_by(Document.name).all()[0]
+    assert doc.name == "md"
+
+    # Check that doc has a figure
+    assert len(doc.figures) == 1
+    assert doc.figures[0].url == "http://placebear.com/200/200"
+
+    # Check that doc has a table
+    assert len(doc.tables) == 1
+    assert doc.tables[0].position == 0
+
+    # Check that doc has cells
+    assert len(doc.cells) == 16
+
+    # Check that doc has phrases
+    assert len(doc.phrases) == 45
 
     logger.info("Doc: {}".format(doc))
     for i, phrase in enumerate(doc.phrases):
@@ -68,64 +83,6 @@ def test_parse_results(caplog):
     # Test lingual attributes
     assert header.ner_tags == ["O", "O"]
     assert header.dep_labels == ["compound", "ROOT"]
-
-    # 45 phrases expected in the "md" document.
-    assert len(doc.phrases) == 45
-
-
-#  def test_parse_structure(caplog):
-#      """Unit test of OmniParserUDF.parse_structure().
-#
-#      This only tests the structural parse of the document.
-#      """
-#      caplog.set_level(logging.INFO)
-#      logger = logging.getLogger(__name__)
-#      Meta.init("postgres://localhost:5432/" + ATTRIBUTE)
-#
-#      max_docs = 1
-#      docs_path = "tests/data/html_simple/md.html"
-#      pdf_path = "tests/data/pdf_simple/md.pdf"
-#
-#      # Preprocessor for the Docs
-#      preprocessor = HTMLDocPreprocessor(docs_path, max_docs=max_docs)
-#
-#      # Grab one document, text tuple from the preprocessor
-#      doc, text = next(preprocessor.generate())
-#      logger.info("    Text: {}".format(text))
-#
-#      # Create an OmniParserUDF
-#      omni_udf = OmniParserUDF(
-#          True,  # structural
-#          ["style"],  # blacklist
-#          ["span", "br"],  # flatten
-#          "",  # flatten delim
-#          True,  # lingual
-#          True,  # strip
-#          [(u"[\u2010\u2011\u2012\u2013\u2014\u2212\uf02d]", "-")],  # replace
-#          True,  # tabular
-#          True,  # visual
-#          pdf_path,  # pdf path
-#          Spacy(),
-#      )  # lingual parser
-#
-#      # Grab the phrases parsed by the OmniParser
-#      phrases = list(omni_udf.parse_structure(doc, text))
-#
-#      logger.warning("Doc: {}".format(doc))
-#      for phrase in phrases:
-#          logger.warning("    Phrase: {}".format(phrase.text))
-#
-#      header = phrases[0]
-#      # Test structural attributes
-#      assert header.xpath == "/html/body/h1"
-#      assert header.html_tag == "h1"
-#      assert header.html_attrs == ["id=sample-markdown"]
-#
-#      # Test the unicode parse of delta
-#      assert phrases[-1].text == "δ13Corg"
-#
-#      # phrases expected in the "md" document.
-#      assert len(phrases) == 45
 
 
 def test_simple_tokenizer(caplog):
@@ -201,6 +158,7 @@ def test_parse_document_md(caplog):
 
     # Grab the md document
     doc = session.query(Document).order_by(Document.name).all()[1]
+    assert doc.name == "md"
 
     logger.info("Doc: {}".format(doc))
     for i, phrase in enumerate(doc.phrases):
@@ -211,6 +169,9 @@ def test_parse_document_md(caplog):
     assert header.xpath == "/html/body/h1"
     assert header.html_tag == "h1"
     assert header.html_attrs == ["id=sample-markdown"]
+
+    # Test the unicode parse of delta
+    assert doc.phrases[-1].text == "δ13Corg"
 
     # Test visual attributes
     assert header.page == [1, 1]
@@ -255,6 +216,7 @@ def test_parse_document_diseases(caplog):
 
     # Grab the diseases document
     doc = session.query(Document).order_by(Document.name).all()[0]
+    assert doc.name == "diseases"
 
     logger.info("Doc: {}".format(doc))
     for phrase in doc.phrases:
@@ -319,59 +281,53 @@ def test_spacy_integration(caplog):
     assert session.query(Phrase).count() == 81
 
 
-#  def test_parse_style(caplog):
-#      """Test style tag parsing."""
-#      caplog.set_level(logging.INFO)
-#      logger = logging.getLogger(__name__)
-#      Meta.init("postgres://localhost:5432/" + ATTRIBUTE)
-#
-#      max_docs = 1
-#      docs_path = "tests/data/html_extended/ext_diseases.html"
-#      pdf_path = "tests/data/pdf_extended/ext_diseases.pdf"
-#
-#      # Preprocessor for the Docs
-#      preprocessor = HTMLDocPreprocessor(docs_path, max_docs=max_docs)
-#
-#      # Grab the document, text tuple from the preprocessor
-#      doc, text = next(preprocessor.generate())
-#      logger.info("    Text: {}".format(text))
-#
-#      # Create an OmniParserUDF
-#      omni_udf = OmniParserUDF(
-#          True,  # structural
-#          [],  # blacklist, empty so that style is not blacklisted
-#          ["span", "br"],  # flatten
-#          "",  # flatten delim
-#          True,  # lingual
-#          True,  # strip
-#          [],  # replace
-#          True,  # tabular
-#          True,  # visual
-#          pdf_path,  # pdf path
-#          Spacy(),
-#      )  # lingual parser
-#
-#      # Grab the phrases parsed by the OmniParser
-#      phrases = list(omni_udf.parse_structure(doc, text))
-#
-#      logger.warning("Doc: {}".format(doc))
-#      for phrase in phrases:
-#          logger.warning("    Phrase: {}".format(phrase.html_attrs))
-#
-#      # Phrases for testing
-#      sub_phrases = [
-#          {
-#              "index": 7,
-#              "attr": [
-#                  "class=col-header",
-#                  "hobbies=work:hard;play:harder",
-#                  "type=phenotype",
-#                  "style=background: #f1f1f1; color: aquamarine; font-size: 18px;",
-#              ],
-#          },
-#          {"index": 10, "attr": ["class=row-header", "style=background: #f1f1f1;"]},
-#          {"index": 12, "attr": ["class=cell", "style=text-align: center;"]},
-#      ]
-#
-#      # Assertions
-#      assert all(phrases[p["index"]].html_attrs == p["attr"] for p in sub_phrases)
+def test_parse_style(caplog):
+    """Test style tag parsing."""
+    caplog.set_level(logging.INFO)
+    logger = logging.getLogger(__name__)
+    session = Meta.init("postgres://localhost:5432/" + ATTRIBUTE).Session()
+
+    # SpaCy on mac has issue on parallel parseing
+    if os.name == "posix":
+        PARALLEL = 1
+    else:
+        PARALLEL = 2  # Travis only gives 2 cores
+
+    max_docs = 1
+    docs_path = "tests/data/html_extended/ext_diseases.html"
+    pdf_path = "tests/data/pdf_extended/ext_diseases.pdf"
+
+    # Preprocessor for the Docs
+    preprocessor = HTMLDocPreprocessor(docs_path, max_docs=max_docs)
+
+    # Create an OmniParser and parse the md document
+    omni = OmniParser(structural=True, lingual=True, visual=True, pdf_path=pdf_path)
+    omni.apply(preprocessor, parallelism=PARALLEL)
+
+    # Grab the document
+    doc = session.query(Document).order_by(Document.name).all()[0]
+
+    # Grab the phrases parsed by the OmniParser
+    phrases = list(doc.phrases)
+
+    logger.warning("Doc: {}".format(doc))
+    for i, phrase in enumerate(phrases):
+        logger.warning("    Phrase[{}]: {}".format(i, phrase.html_attrs))
+
+    # Phrases for testing
+    sub_phrases = [
+        {
+            "index": 5,
+            "attr": [
+                "class=col-header",
+                "hobbies=work:hard;play:harder",
+                "type=phenotype",
+                "style=background: #f1f1f1; color: aquamarine; font-size: 18px;",
+            ],
+        },
+        {"index": 8, "attr": ["class=row-header", "style=background: #f1f1f1;"]},
+        {"index": 10, "attr": ["class=cell", "style=text-align: center;"]},
+    ]
+
+    # Assertions
+    assert all(phrases[p["index"]].html_attrs == p["attr"] for p in sub_phrases)
